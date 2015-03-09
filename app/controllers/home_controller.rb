8afc82ef696794b575bfaf8580fa0ee3e27745f6
Ubuntu <ubuntu@ip-172-31-34-20.us-west-2.compute.internal>
2015-03-09 22:31:08 +0000

    small fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,9 @@ class HomeController < ApplicationController
   def index
   end
 
+  def callback
+    logger.info(request.env['omniauth.auth'])
+    redirect_to '/'
+  end
+
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shibboleth, {
-    :uid_field                 => "uid",
+    :uid_field                 => "eppn",
     :name_field                => "displayName",
     :info_fields => {
       :email    => "mail",
